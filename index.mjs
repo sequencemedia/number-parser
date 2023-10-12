b8193c74637fbefc11e2e8226e7dae98db55cd10
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-10-12 08:52:17 +0100

    Initial commit

---
+++ index.mjs
@@ -0,0 +1,63 @@
+/**
+ * https://observablehq.com/@mbostock/localized-number-parsing
+ */
+export default class NumberParser {
+  constructor (locale = 'en-GB') {
+    const PARTS = (
+      new Intl.NumberFormat(locale, { useGrouping: true }) // true is default
+    ).formatToParts(12345.6)
+
+    const CHARS = (
+      new Intl.NumberFormat(locale, { useGrouping: false })
+    ).format(9876543210).split('').reverse()
+
+    const {
+      value: GROUP
+    } = PARTS.find(({ type }) => type === 'group')
+
+    const {
+      value: DECIMAL
+    } = PARTS.find(({ type }) => type === 'decimal')
+
+    const charsMap = new Map(CHARS.map((d, n) => [d, n]))
+    const group = new RegExp(`[${GROUP}]`, 'g')
+    const decimal = new RegExp(`[${DECIMAL}]`)
+    const chars = new RegExp(`[${CHARS.join('')}]`, 'g')
+
+    function fromChar (char) {
+      return (
+        charsMap.get(char)
+      )
+    }
+
+    function parse (string) {
+      const number = string.trim()
+        .replace(group, '')
+        .replace(decimal, '.')
+        .replace(chars, fromChar)
+
+      return (
+        number
+          ? Number(number)
+          : NaN
+      )
+    }
+
+    this.fromChar = fromChar
+    this.parse = parse
+  }
+}
+
+export function fromString (char) {
+  const numberParser = new NumberParser()
+  return (
+    numberParser.fromChar(String(char)) ?? NaN
+  )
+}
+
+export function toNumber (string) {
+  const numberParser = new NumberParser()
+  return (
+    numberParser.parse(String(string))
+  )
+}
