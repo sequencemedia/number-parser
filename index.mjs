7b40f12a2efb5ec59bb0f9e791208c2f22e44fa1
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-10-12 09:05:34 +0100

    Initial commit

--- index.mjs
+++ index.mjs
@@ -7,22 +7,22 @@ export default class NumberParser {
       new Intl.NumberFormat(locale) // `{ useGrouping: true }` is default
     ).formatToParts(12345.6)
 
-    const CHARS = (
-      new Intl.NumberFormat(locale, { useGrouping: false })
-    ).format(9876543210).split('').reverse()
+    const {
+      value: DECIMAL
+    } = PARTS.find(({ type }) => type === 'decimal')
 
     const {
       value: GROUP
     } = PARTS.find(({ type }) => type === 'group')
 
-    const {
-      value: DECIMAL
-    } = PARTS.find(({ type }) => type === 'decimal')
+    const CHARS = (
+      new Intl.NumberFormat(locale, { useGrouping: false })
+    ).format(9876543210).split('').reverse()
 
-    const charsMap = new Map(CHARS.map((d, n) => [d, n]))
     const group = new RegExp(`[${GROUP}]`, 'g')
     const decimal = new RegExp(`[${DECIMAL}]`)
     const chars = new RegExp(`[${CHARS.join('')}]`, 'g')
+    const charsMap = new Map(CHARS.map((d, n) => [d, n]))
 
     function fromChar (char) {
       return (
